a590537132be8f8a457636d7cfe6e613eea95538
Mugwaneza Joseph <muganezajoseph@gmail.com>
2022-08-11 05:45:16 +0200

    worked on the form of the meme

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Header(){
     return(
         <header className='meme-header'>
-            <img src={require("../images/troll-face.png")} className="troll-face"/>
+            <img src={require("../images/troll-face.png")} className="troll-face" alt='troll-face'/>
             <h2 className='meme--title'>Meme Generator</h2>
             <h4 className='meme--project-name'>React Course  - Project 3</h4>
         </header>
