b49ffbb830c1277e5937e4b587069ad77784138a
Mugwaneza Joseph <muganezajoseph@gmail.com>
2022-08-11 05:06:08 +0200

    worked header design

---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Header(){
+    return(
+        <header className='meme-header'>
+            <img src={require("../images/troll-face.png")} className="troll-face"/>
+            <h2 className='meme--title'>Meme Generator</h2>
+            <h4 className='meme--project-name'>React Course  - Project 3</h4>
+        </header>
+    )
+}
