b85a4e6d18431b0ab51b9cf976d32ebde9f7fb61
Mugwaneza Joseph <muganezajoseph@gmail.com>
2022-08-12 21:53:48 +0200

    working on meme generation

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import memesData from '../memesData';
 
 export default function Meme(){
+    function getMemeImage(){
+        const image = memesData.data.memes.map(meme => meme.url)
+        console.log(image);
+    }
+
     return(
         <main>
-            <form className='meme--form'>
+            <div className='meme--form'>
                 <input type="text" placeholder='Top text' className='form--input'/>
                 <input type="text" placeholder='Bottom text' className='form--input'/>
-                <button className='form--button'>Get a new meme image 🖼</button>
+                <button onClick={getMemeImage} className='form--button'>Get a new meme image 🖼</button>
                 
-            </form>
+            </div>
         </main>
         
     )
