ba22ff999f2aca56274774496bf841212d66cea7
Mugwaneza Joseph <muganezajoseph@gmail.com>
2022-08-15 05:45:14 +0200

    update the state when random image is generated and render it

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -3,11 +3,14 @@ import memesData from '../memesData';
 
 export default function Meme(){
 
+    const [memeImage, setMemeImage] = React.useState("")
+
     function getMemeImage(){
         const images = memesData.data.memes.map(meme => meme.url)
         const randomImageUrl = Math.floor( Math.random()*images.length);
-        console.log(images[randomImageUrl]);
+        setMemeImage(images[randomImageUrl])
     }
+
 
     return(
         <main>
@@ -15,8 +18,8 @@ export default function Meme(){
                 <input type="text" placeholder='Top text' className='form--input'/>
                 <input type="text" placeholder='Bottom text' className='form--input'/>
                 <button onClick={getMemeImage} className='form--button'>Get a new meme image 🖼</button>
-                
             </div>
+            <img src={memeImage} className="meme--image"/>
         </main>
         
     )
