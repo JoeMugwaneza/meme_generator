8b41921498d7a7958245da16956fd37eca0dab24
Mugwaneza Joseph <muganezajoseph@gmail.com>
2022-08-18 20:33:16 +0200

    worked on refactoring meme code

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -3,14 +3,29 @@ import memesData from '../memesData';
 
 export default function Meme(){
 
-    const [memeImage, setMemeImage] = React.useState("")
+    const [memeImage, setMemeImage] = React.useState("http://i.imgflip.com/1bij.jpg");
+    
+    const [meme, setMeme] = React.useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "http://i.imgflip.com/1bij.jpg"
+    });
+
+    const [allMemeImages, setAllMemeImages] = React.useState(memesData);
 
     function getMemeImage(){
         const images = memesData.data.memes.map(meme => meme.url)
-        const randomImageUrl = Math.floor( Math.random()*images.length);
-        setMemeImage(images[randomImageUrl])
-    }
+        const randomImageUrl = Math.floor(Math.random()*images.length);
+        const url = images[randomImageUrl]; 
 
+        setMeme(prevMeme => ({ 
+            ...prevMeme, 
+            randomImage: url
+        }))
+
+        setMemeImage(images[randomImageUrl])
+
+    }
 
     return(
         <main>
@@ -19,7 +34,7 @@ export default function Meme(){
                 <input type="text" placeholder='Bottom text' className='form--input'/>
                 <button onClick={getMemeImage} className='form--button'>Get a new meme image 🖼</button>
             </div>
-            <img src={memeImage} className="meme--image"/>
+            <img src={meme.randomImage} className="meme--image"/>
         </main>
         
     )
