a362d630b95a110e9eb924db4e6a49e1cfeadabb
Mugwaneza Joseph <muganezajoseph@gmail.com>
2022-08-26 05:03:03 +0200

    ability to capture top and bottom text on the picture

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -11,6 +11,17 @@ export default function Meme(){
         randomImage: "http://i.imgflip.com/1bij.jpg"
     });
 
+    console.log(meme)
+
+    function handleChange(event){
+        const {name, value, type} = event.target
+
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            [name]: value
+        }))
+    }
+
     const [allMemeImages, setAllMemeImages] = React.useState(memesData);
 
     function getMemeImage(){
@@ -30,11 +41,36 @@ export default function Meme(){
     return(
         <main>
             <div className='meme--form'>
-                <input type="text" placeholder='Top text' className='form--input'/>
-                <input type="text" placeholder='Bottom text' className='form--input'/>
-                <button onClick={getMemeImage} className='form--button'>Get a new meme image 🖼</button>
+
+                <input 
+                    type="text" 
+                    placeholder='Top text' 
+                    className='form--input'
+                    name='topText'
+                    onChange={handleChange}
+                />
+
+                <input 
+                    type="text"
+                    placeholder='Bottom text'
+                    className='form--input'
+                    name='bottomText'
+                    onChange={handleChange}
+                />
+
+                <button 
+                    onClick={getMemeImage} 
+                    className='form--button'>
+                    Get a new meme image 🖼
+                </button>
+
             </div>
-            <img src={meme.randomImage} className="meme--image"/>
+
+            <div className='meme'>
+                <img src={meme.randomImage} className="meme--image"/>
+                <h2 className='meme--text top'>One does not simply</h2>
+                <h2 className='meme--text bottom'>Walk into Mordor</h2>
+            </div>
         </main>
         
     )
